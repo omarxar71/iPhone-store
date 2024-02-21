5ddb42fb94a0b4625bdad6a1b768e247ff7ec8f0
omarxar71 <115642673+omarxar71@users.noreply.github.com>
2024-02-21 07:50:21 +0200

    Add files via upload

---
+++ iphone selling store/main.js
@@ -0,0 +1,8 @@
+let main = document.getElementById('main')
+let container = document.querySelector('.container')
+function phones(phone){
+    main.src= phone;
+}
+function colors (color){
+    container.style.background= color
+}
